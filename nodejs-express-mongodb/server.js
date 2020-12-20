d5369ce0d67048cd626f7de5df50177f1e139a71
Raymond Huang <chialh@uci.edu>
2020-12-19 22:23:22 -0800

    :poop:

--- nodejs-express-mongodb/server.js
+++ nodejs-express-mongodb/server.js
@@ -3,3 +3,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
