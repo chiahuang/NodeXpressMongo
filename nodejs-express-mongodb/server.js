15558a365939667b7dc437716ed8ca8c30744203
Raymond Huang <chialh@uci.edu>
2020-12-20 21:56:38 -0800

    Add more :poop:

--- nodejs-express-mongodb/server.js
+++ nodejs-express-mongodb/server.js
@@ -4,6 +4,11 @@ const cors = require("cors");
 
 const app = express();
 
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to simple application." });
+});
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
